09dc76615a57c8958255c8cb92f4a010cf4f3e24
Kevin Pignoly <pignoly.kevin@gmail.com>
2024-05-30 09:51:07 +0200

    settings

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -16,7 +16,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env(env_file=str(BASE_DIR / "mercadona" / ".env"))
+environ.Env.read_env(env_file=str(BASE_DIR / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
