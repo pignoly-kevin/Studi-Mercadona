ae40cce964e08ad7fbd9533a250576607ff01c19
Kevin Pignoly <pignoly.kevin@gmail.com>
2024-05-30 09:24:12 +0200

    settings

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'mercadona.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mercadona',
+        'USER': 'mercadona',
+        'PASSWORD': 'Mercadona',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
